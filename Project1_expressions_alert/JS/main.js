1d861bcfed2810a22d0491dbe9779f061ea2a035
gidka1998 <gidka1998@gmail.com>
2026-05-29 13:40:49 +0100

    Project 1

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+// This variable stores the beginning of the sentence 
+var Sent1 = "This is the beginning of the string";
+
+// This variable stores the end of the sentence 
+var Sent2 = " and this is the end of the string";
+
+// Concatenating Sent1 and Sent2 together
+var fullSentence = Sent1 + Sent2;
+
+// Displaying the concatenated sentence in an alert box
+window.alert(fullSentence);
+
+// document.write() is deprecated in modern JavaScript,
+// but it is used here because it is required for this assignment.
+document.write(fullSentence);
